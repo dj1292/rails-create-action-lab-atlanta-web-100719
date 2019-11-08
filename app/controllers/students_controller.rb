6ec6651db8bd54bd73ca08c38f520d6704221d54
Dan Reeves <you@example.com>
2019-11-08 14:29:07 -0500

    Almost done

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -9,9 +9,24 @@ class StudentsController < ApplicationController
   end
 
   def new
+    @student = Student.new 
+    # byebug
   end
 
   def create
+    @student = Student.create(
+      {
+        first_name: params[:first_name],
+        last_name: params[:last_name]
+      }
+    )
+    redirect_to student_path(@student)
   end
+
+  private
+
+  def student_params
+    strong_params = params.require(:students).permit(:first_name, :last_name)
+  end 
 
 end
